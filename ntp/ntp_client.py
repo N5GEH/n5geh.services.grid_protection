4441cc22f85271ed02f52bb9dfc2cc0e2fc4bca9
KrahmerTUD <sebastian.krahmer@tu-dresden.de>
2019-12-17 12:18:40 +0100

    add ntp client

---
+++ ntp/ntp_client.py
@@ -0,0 +1,28 @@
+#  Copyright (c) 2019.
+#  Author: Sebastian Krahmer
+
+"""This is a simple NTP-Client.
+
+This module requests the actual time from a ntp server and format to datetime string.
+    Methods:
+            getNTPDateTime(server_adress)
+"""
+import datetime
+import ntplib
+
+def getNTPDateTime(server):
+    try:
+        ntpDate = None
+        client = ntplib.NTPClient()
+        response = client.request(server, version=3)
+        ntpDate = response.tx_time
+        # print(ntpDate)
+        # print(response.offset)
+    except Exception as e:
+        print(e)
+    print(datetime.datetime.fromtimestamp(ntpDate).strftime("%Y-%m-%d %H:%M:%S.%f"))
+    return datetime.datetime.fromtimestamp(ntpDate).strftime("%Y-%m-%d %H:%M:%S.%f")
+
+
+if __name__ == "__main__":
+    getNTPDateTime('ubuntu5g.etieeh.loc')
